7bcd4578c8c32a0e2167ffa89705df0d9b98654f
Acosta <79601880+Acosta-gh@users.noreply.github.com>
2026-05-09 03:21:31 -0300

    Se agrego redirect

--- next.config.mjs
+++ next.config.mjs
@@ -12,6 +12,20 @@ const nextConfig = {
   compress: true,
   poweredByHeader: false,
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/events',
+        destination: 'https://www.zeffy.com/en-CA/ticketing/the-trevor-claydon-fundraiser',
+        permanent: false,
+      },
+      {
+        source: '/event',
+        destination: 'https://www.zeffy.com/en-CA/ticketing/the-trevor-claydon-fundraiser',
+        permanent: false,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
